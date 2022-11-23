ba6a6670b25eb7541d50241837730679586b3f03
leylshafi <llshafi03@gmail.com>
2022-11-23 16:40:13 +0400

    Add project files.

---
+++ BridgeDesignPattern/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Bridge;
+
+
+// Implementation
+interface IColor
+{
+    string? Name { get; set; }
+    (int, int, int) GetRGB();
+}
+
+
+
+// Concrete implementation 1
+class Red : IColor
+{
+    public string? Name { get; set; } = "Red";
+    public (int, int, int) GetRGB() => (255, 0, 0);
+}
+
+
+// Concrete implementation 2
+
+class Blue : IColor
+{
+    public string? Name { get; set; } = "Blue";
+    public (int, int, int) GetRGB() => (0, 0, 255);
+}
+
+
+
+// Abstraction
+abstract class Shape
+{
+    public IColor? Color { get; set; }
+
+    public double Area { get; protected set; }
+    public byte Corner { get; init; }
+    public string? Name { get; set; }
+
+
+    protected Shape(IColor? color, double area, byte corner, string? name)
+    {
+        Color = color;
+        Area = area;
+        Corner = corner;
+        Name = name;
+    }
+}
+
+
+
+// Refined abstraction (optional)
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(IColor color, double width, double height)
+        : base(color, width * height, 4, nameof(Rectangle))
+    {
+        Width = width;
+        Height = height;
+    }
+
+}
+
+
+
+// Refined abstraction (optional)
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+
+    public Circle(IColor color, double radius)
+        : base(color, 3.14 * Math.Pow(radius, 2), 0, nameof(Circle))
+    {
+        Radius = radius;
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        IColor color = new Red();
+        // color = new Blue();
+
+        Shape shape = new Rectangle(color, 10, 7);
+        shape = new Circle(color, 10);
+
+
+        Console.WriteLine(shape.Name);
+        Console.WriteLine(shape.Area);
+        Console.WriteLine(shape.Corner);
+        Console.WriteLine(shape.Color?.Name);
+
+
+        Console.WriteLine(shape.Color?.GetRGB());
+    }
+}
---
+++ Task/Program.cs
@@ -0,0 +1,185 @@
+﻿public abstract class Remote
+{
+    protected IDevice Device { get; set; }
+    public Remote(IDevice device)
+    {
+        Device=device;
+    }
+    public void togglePower()
+    {
+        if (Device.IsEnabled())
+            Device.Disable();
+        else 
+            Device.Enable();
+    }
+    public void volumeDown()
+    {
+        int current = Device.GetVolume();
+        Device.SetVolume(--current);
+    }
+    public void volumeUp()
+    {
+        int current = Device.GetVolume();
+        Device.SetVolume(++current);
+    }
+    public void channelDown()
+    {
+        int current = Device.GetChannel();
+        Device.SetChannel(--current);
+    }
+    public void channnelUp()
+    {
+        int current = Device.GetChannel();
+        Device.SetChannel(++current);
+    }
+}
+
+public class AdvancedRemote:Remote
+{
+
+    public AdvancedRemote(IDevice device) : base(device)
+    {
+
+    }
+
+    public void Mute()
+    {
+        Device.SetVolume(0);
+    }
+}
+
+public interface IDevice
+{
+
+    bool IsEnabled();
+    int GetVolume();
+    void SetVolume(int num);
+    int GetChannel();
+    void SetChannel(int num);
+    void Enable();
+    void Disable();
+}
+
+public class Radio : IDevice
+{
+    public bool IsEnable = false;
+    public int Volume=0;
+    public int Channel=0;
+
+    public void Disable()
+    {
+        IsEnable = false;
+    }
+
+    public void Enable()
+    {
+        IsEnable = true;
+    }
+
+    public int GetChannel()
+    {
+       return Channel;
+    }
+
+    public int GetVolume()
+    {
+        return Volume;
+    }
+
+    public bool IsEnabled()
+    {
+       return IsEnable;
+    }
+
+    public void SetChannel(int num)
+    {
+        Channel = num;
+    }
+
+    public void SetVolume(int num)
+    {
+        Volume = num;
+    }
+    public override string ToString() =>
+$"Device {nameof(Radio)},\n" +
+$"Is Enabled - {IsEnable},\n" +
+$"Volume - {Volume},\n" +
+$"Channel - {Channel}";
+}
+
+
+public class TV : IDevice
+{
+    public bool IsEnable = false;
+    public int Volume=0;
+    public int Channel=0;
+
+    public void Disable()
+    {
+        IsEnable = false;
+    }
+
+    public void Enable()
+    {
+        IsEnable = true;
+    }
+
+    public int GetChannel()
+    {
+        return Channel;
+    }
+
+    public int GetVolume()
+    {
+        return Volume;
+    }
+
+    public bool IsEnabled()
+    {
+        return IsEnable;
+    }
+
+    public void SetChannel(int num)
+    {
+        Channel = num;
+    }
+
+    public void SetVolume(int num)
+    {
+        Volume = num;
+    }
+    public override string ToString() =>
+$"Device {nameof(TV)},\n" +
+$"Is Enabled - {IsEnable},\n" +
+$"Volume - {Volume},\n" +
+$"Channel - {Channel}";
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        IDevice device = new Radio();
+        device.SetVolume(50);
+        device.SetChannel(22);
+
+        Remote remote = new AdvancedRemote(device);
+        remote.channnelUp();
+        remote.volumeDown();
+
+        Console.WriteLine(device);
+
+
+        device = new TV();
+        device.SetVolume(30);
+        device.SetChannel(45);
+
+        remote = new AdvancedRemote(device);
+        remote.togglePower();
+        remote.volumeUp();
+        remote.channnelUp();
+        Console.WriteLine();
+        Console.WriteLine(device);
+    }
+}
